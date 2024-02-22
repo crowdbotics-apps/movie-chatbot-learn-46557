e18a7ce2d8ed49994d27d498eab97163db43c607
Crowdbotics <team@crowdbotics.com>
2024-02-22 04:16:24 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "movie-chatbot-learn-46557",
+  url: "https://movie-chatbot-learn-46557.botics.co",
+  api: "https://movie-chatbot-learn-46557.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
